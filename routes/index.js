1b2d929255408083fed2d61c2da28864f0105869
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-08-31 20:51:28 -0300

    exercicio cms

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var cms = require('./../data/cms');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+      title: "Express", 
+      app_nome: cms.global.app_name,
+      texto: cms.home.texto_introducao
+    });
 });
 
 module.exports = router;
