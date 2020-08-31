3440d66b23ad164c93426c5f6b1036003584f903
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-08-31 20:00:07 -0300

    CMS

---
+++ data/cms.js
@@ -0,0 +1,8 @@
+module.exports = {
+    global: {
+        app_name: 'PETZÃO'
+    },
+    home: {
+        texto_introducao: "Os Melhores Produtos e Acessórios para Seu Pet estão na Petlove. Qualidade e Preço Justo! Petlove - Tudo que Seu Pet Precisa! Confira Produtos de Qualidade com Preços Especiais. Desde 1999. Compre no App Petlove. Compra Segura." 
+    }
+}
