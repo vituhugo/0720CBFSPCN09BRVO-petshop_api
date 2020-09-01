e14b32c286e818a3d9c695071f95ebef21129228
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-08-31 21:22:00 -0300

    exercicio cms

--- routes/dono.js
+++ routes/dono.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var donos = require('./../data/dono');
 
 router.get('/', function(req, res, next) {
-    res.render('dono/lista');
+    res.render('dono/lista', {donos});
 });
 router.get('/:id', function(req, res, next) {
     res.render('dono/show');
