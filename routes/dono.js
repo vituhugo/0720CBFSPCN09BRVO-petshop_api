ee91edd460a9521e12d6a20232864b7f86d02555
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-09-04 19:46:52 -0300

    exemplo

--- routes/dono.js
+++ routes/dono.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router();
 var donos = require('./../data/dono');
 
+// router.METODO("ENDERECO", "FUNCAO_ACAO")
 router.get('/', function(req, res, next) {
     res.render('dono/lista', {donos});
 });
+
 router.get('/:id', function(req, res, next) {
+    const id = req.params.id;
+    console.log("ID DA REQUISICAO:", id);
     res.render('dono/show');
 });
     
