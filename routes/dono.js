1bf8be58ce863eeb19324998eef7e9c148400a16
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-11-27 18:29:52 +0000

    api

--- routes/dono.js
+++ routes/dono.js
@@ -5,38 +5,28 @@ var donos = require('./../data/dono');
 // router.METODO("ENDERECO", "FUNCAO_ACAO")
 router.get('/', function(req, res, next) {
     
-    res.render('dono/lista', {donos});
+    res.status(200).json(donos);
 });
 
-router.get('/:id/visualizar', function(req, res, next) {
+router.get('/:id', function(req, res, next) {
     const id = req.params.id;
-    console.log("ID DA REQUISICAO:", id);
     
     let dono = donos.find(dono => dono.id == id);
-    console.log("DONO:", dono);
     
-    res.render('dono/show', {dono});
+    res.status(200).json(dono);
 });
 
-router.get('/:id/editar', function(req, res, next) {
-    const id = req.params.id;
-
-    let dono = donos.find(dono => dono.id == id);
-
-    res.render('dono/editar', {dono});
-});
-
-router.put('/:id/alterar', function(req, res, next) {
+router.put('/:id', function(req, res, next) {
     const id = req.params.id;
     let dono = donos.find(dono => dono.id == id);
     dono.email = req.body.email;
     dono.nome = req.body.nome;
     dono.data_nascimento = req.body.data_nascimento;
 
-    res.redirect('/dono');
+    res.status(200).json(dono);
 });
 
-router.delete('/:id/remover', function(req, res, next) {
+router.delete('/:id', function(req, res, next) {
     const id = req.params.id;
     const index = donos.findIndex((dono) => dono.id == id);
 
@@ -44,24 +34,22 @@ router.delete('/:id/remover', function(req, res, next) {
 
     donos.splice(index, 1);
 
-    res.redirect('/dono');
+    res.status(203).json(null);
 });
 
-router.get('/criar', function(req, res, next) {
-    res.render('dono/criar');
-})
-
-router.post('/guardar', function(req, res, next) {
+router.post('/', function(req, res, next) {
     let {email, data_nascimento, nome} = res.body;
     
-    donos.push({
+    let novo_dono = {
         id: donos.reverse()[0].id+1, 
         email, 
         data_nascimento, 
         nome
-    })
+    };
 
-    res.redirect('/dono');
+    donos.push(novo_dono)
+
+    res.status(201).json(novo_dono);
 })
 
 module.exports = router;
